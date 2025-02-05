8faa6b3762afc7c27f6b75b301db03a129cd783c
Kimberly Yen <kimberly.yen@bqool.com>
2025-02-05 11:30:22 +0800

    body bgc change together

--- src/PickUpColor.jsx
+++ src/PickUpColor.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const PickUpColor = () => {
-
-  const [ color , setColor] =  useState('#FFFFFF')
+  const [color, setColor] = useState("#FFFFFF");
 
   const handleColor = (e) => {
-    setColor(e.target.value)
-  }
-    
+    setColor(e.target.value);
+  };
+
   return (
     <div>
-      <div className="color-box" id="colorBox"></div>
-      <input type="color" id="colorPicker" value={ color } onChange={handleColor}/>
-      <p>選擇的顏色: <span id="colorValue">{ color }</span></p>
+      <body style={{backgroundColor: color}}>
+        <div className="color-box" id="colorBox"></div>
+        <input type="color" id="colorPicker" value={color} onChange={handleColor} />
+        <p>
+          選擇的顏色: <span id="colorValue">{color}</span>
+        </p>
+      </body>
     </div>
-  )
-}
+  );
+};
 
-export default PickUpColor
+export default PickUpColor;
